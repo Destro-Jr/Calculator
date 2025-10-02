437d8f85c02bd0ee3bdf69f010390377cade7440
Destro-Jr <nwoyedestiny314@gmail.com>
2025-10-02 07:26:32 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+# name = "david"
+# if name:
+#     # print("david exist")
+#     if len(name) >4:
+#         print("yeah we can procede")
+#         if name.isalpha():
+#             print(f"welcome finaly{name}")
+#         else:
+#             print("please go back")    
+#     else:
+#         print:("we cannot use you")    
+# else:
+#     print("david is not here")
+
+
+
+
+
+# names = ["shegun", "john", "david", "mary","vic"]
+# cars = ("toyota", "honda", "dodge", "ford")
+
+# names[4]="precious"
+# prin(names)
+# list_cars = list(cars)
+# list_cars[1]="bmw"
+# # print(cars)\
+# tuple_cars = tuple(list_cars)
+# print(tuple_cars)
+
+
+
+# scores = [30,58,90,20,67,18,76,99,26,45]
+# minimum_scores = min(scores)
+# maximum_scores = max(scores)
+# print(minimum_scores)
+
+
+
+
+# names = ["shegun","david", ]
+
+
+
+num1 = int(input("Enter first number: "))
+operation = input("Enter the operation: ")
+num2 = int(input("Enter second number: "))
+
+
+if operation == "+": 
+    print(num1+num2)
+if operation == "-":
+    print(num1-num2)
+if operation =="/":
+    print(num1/num2)
+if operation == "*":
+    print(num1*num2)
